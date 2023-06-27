7002b03eea99da16f33bdd6785593dfd0dc6a224
PisitTr65 <pisit.tr.65@ubu.ac.th>
2023-06-27 15:43:56 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+function App() {
+  return (
+    <>
+    <div className='card'>
+      <Avatar />
+      <div className='data'>
+        <Intro />
+        <SkillList />
+      </div>
+    </div>   
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className='skill-list'>
+      <Skill Skill="Unity" emoji="💪" color="cyan" />
+      <Skill Skill="PHP" emoji="💪" color="orange" />
+      <Skill Skill="React" emoji="🧒" color="yellow" />
+      <Skill Skill="MySQL" emoji="👍" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className='skill' style={{ backgroundColor: props.color }}>
+      <span>{props.Skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>นายพิสิษฐ์ ตรีรัตน์วิชชา</h1>
+      <p>
+        นักศึกษาสาขาเทคโนโลยีสารสนเทศและการสื่อสาร คณะวิทยาศาสตร์
+        มหาวิทยาลัยอุบลราชธี ชื่นชอบในการตัดต่อ
+        งานอดิเรก อ่านนิยาย ฟังเพลง
+      </p>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className='avatar' src="GitHub-Mark.png" alt="My Avatar" />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +61,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
